2b6fcfc8dae93998d0d97d492917ea782e4ce58a
JehanKandy <jehankandy@gmail.com>
2023-01-17 20:37:10 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -1,14 +1,22 @@
 <link rel="stylesheet" href="../../css/style.css">
-<div class="footer-main">
-    <div class="container">
-        <div class="food-grid">
-            <div class="itme1">JK Catering</div>
-            <div class="itme2"></div>
-            <div class="itme3"></div>
-            <div class="itme4"></div>
+<footer>
+    <div class="footer-main">
+        <div class="container">
+            <div class="food-grid">
+                <div class="itme1">
+                    <div class="title">JK Catering</div>
+                </div>
+                <div class="itme2">
+                    <div class="title">Usefull Links</div>
+                </div>
+                <div class="itme3">
+                    <div class="title">Our Services</div>
+                </div>
+                <div class="itme4">
+                    <div class="title">Contact</div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-
+</footer>
 <script src="../../js/script.js"></script>
-
