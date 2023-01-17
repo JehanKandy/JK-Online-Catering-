2725cd37ff0bbde464e60fd3b57f8f931123a497
JehanKandy <jehankandy@gmail.com>
2023-01-17 20:16:51 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -1,7 +1,12 @@
 <link rel="stylesheet" href="../../css/style.css">
 <div class="footer-main">
     <div class="container">
-        
+        <div class="food-grid">
+            <div class="itme1">Item 1</div>
+            <div class="itme2">Item 2</div>
+            <div class="itme3">Item 3</div>
+            <div class="itme4">Item 4</div>
+        </div>
     </div>
 </div>
 
