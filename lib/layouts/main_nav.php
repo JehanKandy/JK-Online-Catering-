122e97f04473e0874cf5a03706eeb9d7911fe145
JehanKandy <jehankandy@gmail.com>
2023-01-18 14:43:08 +0530

    Update main_nav.php

--- lib/layouts/main_nav.php
+++ lib/layouts/main_nav.php
@@ -6,7 +6,7 @@
             <div class="logo-text">JK Caterings</div>
 
             <div class="nav-right">
-                hi
+                <a href=""><button class="login-nav-btn">Login</button></a>
             </div>
         </div>
     </div>
