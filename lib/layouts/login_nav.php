0c731633b757832a054d7459ab736c0d597349d9
JehanKandy <jehankandy@gmail.com>
2023-01-18 20:35:12 +0530

    Add files via upload

---
+++ lib/layouts/login_nav.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="../../css/style.css">
+<nav>
+    <div class="nav-contenmt">
+        <div class="container">
+            <img src="images/fried_rice.jpg" alt="" class="nav-logo">
+            <div class="logo-text">JK Caterings</div>
+
+        </div>
+    </div>
+</nav>
+
+
+<script src="../../js/script.js"></script>
