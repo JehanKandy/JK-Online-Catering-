f0ade488f0f6d9dc9e637095232697d2332b372d
JehanKandy <jehankandy@gmail.com>
2023-01-18 18:24:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 <?php include("lib/layouts/main_nav.php"); ?>
 
 <div class="home-content">
-    <div class="title">JK Catering Service</div>
+    <div class="title">JK Catering Service <br>
+    <a href=""><button class="home-info-btn">Read More</button></a>
+    </div>
+</div>
+
+<div class="main-content">
+    <div class="container">
+        hi
+    </div>
 </div>
 
 <?php //include("lib/layouts/main_footer.php"); ?>
