0de54b53bda34063d0fd1e77f6e78c0db10ec267
JehanKandy <jehankandy@gmail.com>
2023-01-18 18:43:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,45 @@
 </div>
 
 <div class="main-content">
-    <div class="container">
         <div class="food-info-grid">
             <div class="fitem1"></div>
-            <div class="fitem2">Food 2</div>
-            <div class="fitem3">Food 3</div>
-            <div class="fitem4">Food 4</div>
+            <div class="fitem2">
+                <div class="title">
+                    Nice Foods
+                </div>
+                <div class="body">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa mollitia error ullam quis suscipit odit soluta deleniti possimus. Iure, aut minus eos nisi voluptates recusandae inventore omnis? Adipisci, vero iusto.
+                    Ipsa quod, alias tempore vero exercitationem, veritatis qui quaerat, ipsam autem amet veniam consequatur cumque repellat enim fuga! Maiores, omnis. Ullam consequatur placeat expedita debitis, facere culpa eligendi dignissimos nobis.
+                    Deleniti, vel fuga accusamus fugit unde pariatur. Dolorum inventore, eum iusto incidunt odio possimus nostrum qui quaerat, consequuntur iure animi exercitationem voluptatem saepe id corrupti repudiandae dolor optio explicabo. Error.
+                    Eligendi rem minus eum in maiores deleniti voluptates vero, enim doloribus repellat optio, quaerat quis recusandae nemo suscipit corporis impedit natus consequuntur sunt maxime. Doloremque officia aspernatur nam qui? Quae?
+                    Ipsum eaque quibusdam exercitationem! A id similique ut, voluptatem odio sapiente quam perspiciatis? Libero quasi natus, doloremque explicabo aliquam ipsum maiores? Corrupti doloremque odit maiores hic beatae, suscipit deleniti dolorem.
+                    Sapiente aliquid ullam corrupti asperiores, dolores maxime sequi dignissimos nesciunt sint cupiditate omnis repellat aspernatur beatae amet unde sed, veritatis corporis minima molestiae deserunt? Magni expedita fuga delectus animi quas?
+                    Fugiat cumque natus cupiditate placeat tempora suscipit voluptatem laboriosam, incidunt dolor sapiente sed sit at deserunt illo odit! Eius debitis sunt exercitationem excepturi fugiat officiis corrupti esse quibusdam architecto omnis!
+                    Eos laudantium quis voluptatibus et reprehenderit alias numquam exercitationem placeat ab labore ratione, culpa quisquam? Id doloremque a molestias distinctio deleniti, ex quo, facere natus dolor debitis velit, quos delectus!
+                    Qui consequatur est iusto ullam, necessitatibus blanditiis odit asperiores distinctio earum at, dolores delectus eius sint architecto, porro eveniet. At libero qui aperiam perspiciatis corporis cupiditate minima repellendus, id quam.
+                    Non facere molestias dicta officiis in praesentium odit tempore earum incidunt, fugit consequatur sit temporibus aliquam vero laudantium dolores, asperiores vitae repellendus laborum voluptatem similique placeat impedit sint! Aut, nulla.
+                </div>
+            </div>
+            <div class="fitem3">
+                <div class="title">Most Popular Foods</div>
+                <div class="body">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit quaerat quis labore placeat, quod minima, incidunt unde repellendus aspernatur laborum maxime a eius exercitationem delectus nisi. Unde natus exercitationem cupiditate.
+                    Tempore quis placeat quaerat quae quam fugiat, mollitia repellendus ullam delectus veniam alias ex reprehenderit possimus, dolores esse magnam sequi harum. Accusamus vitae perferendis totam alias delectus cupiditate harum officia.
+                    Voluptate nobis asperiores, in odio at ad distinctio, laboriosam placeat aut est pariatur sint quo. Iusto molestiae maxime ducimus. Impedit quasi eveniet hic suscipit odit earum esse illo nesciunt aut!
+                    Quaerat sed atque ullam fuga? Maxime veniam voluptate accusantium. Nisi quod vitae beatae sit sequi pariatur, molestias doloremque dolore voluptatibus perferendis voluptates asperiores sunt molestiae libero iste et cupiditate unde!
+                    Eos excepturi dolores veritatis alias fugiat. Sint, expedita. Dolor fuga ipsum adipisci, nihil amet architecto dicta quia doloremque suscipit eum obcaecati dolores quae consequatur, rem doloribus ea placeat, sed accusantium.
+                    Delectus dignissimos quasi similique, est optio mollitia fugiat porro, recusandae non neque animi, eius corrupti aliquid ad! Laborum facilis obcaecati qui incidunt in, rem recusandae reprehenderit magni amet aliquid cum!
+                    Cum repellat aspernatur doloremque! Nemo, quas eaque facilis cupiditate eos illo culpa similique nobis tenetur deleniti beatae esse voluptate est aut minima nisi asperiores amet libero! Inventore atque omnis quod.
+                    Ab in saepe distinctio dolore eos cum quisquam, alias quasi exercitationem, repellendus soluta debitis enim itaque consequuntur ad laboriosam temporibus perspiciatis ea impedit. Nemo saepe quisquam aspernatur perspiciatis maiores officia.
+                    Officia inventore cupiditate itaque eaque deleniti rem. Ad, fugit. Pariatur, itaque reprehenderit? Alias recusandae sapiente amet delectus reprehenderit corrupti culpa! In iusto assumenda excepturi nisi fuga vitae, tenetur ipsum aliquam.
+                    Saepe facilis aut dolorem tempora odit dolore, quisquam ipsum fugit. Eaque, autem. Est, omnis, accusamus neque expedita quo hic tempora quod itaque, soluta quasi ad doloribus unde dolorum sapiente praesentium?
+                </div>
+            </div>
+            <div class="fitem4"></div>
         </div>
-    </div>
 </div>
+
+
 
 <?php //include("lib/layouts/main_footer.php"); ?>
 <script src="js/script.js"></script>
