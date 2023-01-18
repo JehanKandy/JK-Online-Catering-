aa1d161a6b8c5e3ae75c711c532676c4c898f3d1
JehanKandy <jehankandy@gmail.com>
2023-01-18 14:53:57 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
 <?php include("lib/layouts/header.php"); ?>
 <?php include("lib/layouts/main_nav.php"); ?>
 
+<div class="home-content">
+    hi all
+</div>
+
 <?php //include("lib/layouts/main_footer.php"); ?>
 <script src="js/script.js"></script>
