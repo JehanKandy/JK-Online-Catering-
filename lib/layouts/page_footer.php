28b5bf39e264769464f2815d2ef4b52081e643f0
JehanKandy <jehankandy@gmail.com>
2023-01-18 20:44:19 +0530

    Update page_footer.php

--- lib/layouts/page_footer.php
+++ lib/layouts/page_footer.php
@@ -2,7 +2,7 @@
 <footer>
     <div class="page-footer">
         <div class="container">
-            
+            All Rights Reserved 2021 - 2023 <i class="far fa-copyright"></i><a href="https://github.com/JehanKandy/JK-Online-Catering-" target="_blank">JK Caterings,</a> Developed By <a href="https://github.com/JehanKandy" target="_blank">JehanKandy</a> 
         </div>
     </div>
 </footer>
