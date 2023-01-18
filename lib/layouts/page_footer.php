f8e90b48a67ef13dc1b9d9f8a88a57350ffdb408
JehanKandy <jehankandy@gmail.com>
2023-01-18 20:43:14 +0530

    Update page_footer.php

--- lib/layouts/page_footer.php
+++ lib/layouts/page_footer.php
@@ -1,3 +1,11 @@
 <link rel="stylesheet" href="../../css/style.css">
+<footer>
+    <div class="page-footer">
+        <div class="container">
+            
+        </div>
+    </div>
+</footer>
+
 
 <script src="../../js/script.js"></script>
