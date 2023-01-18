697f8dbcc5682f2fed2ef82e41654670e4b53fde
JehanKandy <jehankandy@gmail.com>
2023-01-18 13:49:00 +0530

    Update main_footer.php

--- lib/layouts/main_footer.php
+++ lib/layouts/main_footer.php
@@ -5,12 +5,33 @@
             <div class="food-grid">
                 <div class="itme1">
                     <div class="title">JK Catering</div>
+                    <div class="body">
+                        Develop for Educational purposes only
+                    </div>
                 </div>
                 <div class="itme2">
                     <div class="title">Usefull Links</div>
+                    <div class="body">
+                        <ul>
+                            <li><a href="#"><i class="fas fa-utensils"></i> &nbsp; Foods</a></li>
+                            <li><a href="#"><i class="fas fa-utensils"></i> &nbsp; Appointment</a></li>
+                            <li><a href="#"><i class="fas fa-utensils"></i> &nbsp; Book Foods</a></li>
+                            <li><a href="#"><i class="fas fa-utensils"></i> &nbsp; Arrange Foods</a></li>
+                            <li><a href="#"><i class="fas fa-utensils"></i> &nbsp; Food Info</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="itme3">
                     <div class="title">Our Services</div>
+                    <div class="body">
+                        <ul>
+                            <li><a href="#"><i class="fas fa-coffee"></i> &nbsp; Catering Service</a></li>
+                            <li><a href="#"><i class="fas fa-coffee"></i> &nbsp;Party </a></li>
+                            <li><a href="#"><i class="fas fa-coffee"></i> &nbsp;Wedding </a></li>
+                            <li><a href="#"><i class="fas fa-coffee"></i> &nbsp;Online Foods</a></li>
+                            <li><a href="#"><i class="fas fa-coffee"></i> &nbsp;Branches</a></li>
+                        </ul>
+                    </div>
                 </div>
                 <div class="itme4">
                     <div class="title">Contact</div>
