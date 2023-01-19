79f7a5305374cb2149a6e14e1219532e002f8d3b
JehanKandy <jehankandy@gmail.com>
2023-01-19 10:43:43 +0530

    Update login_nav.php

--- lib/layouts/login_nav.php
+++ lib/layouts/login_nav.php
@@ -2,8 +2,9 @@
 <nav>
     <div class="nav-contenmt">
         <div class="container">
-            <img src="images/fried_rice.jpg" alt="" class="nav-logo">
+            <img src="../../images/fried_rice.jpg" alt="" class="nav-logo">
             <div class="logo-text">JK Caterings</div>
+
         </div>
     </div>
 </nav>
