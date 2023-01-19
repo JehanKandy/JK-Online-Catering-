291888d2346e7d171eed619d32f3a2ef5e877b25
JehanKandy <jehankandy@gmail.com>
2023-01-19 11:09:00 +0530

    Update login.php

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,6 +1,12 @@
 <link rel="stylesheet" href="../../css/style.css">
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/login_nav.php"); ?>
-<br>
+
+<div class="login-content">
+    <div class="container">
+        hi
+    </div>
+</div>
+
 <?php include("../layouts/page_footer.php"); ?>
 <script src="../../js/script.js"></script>
