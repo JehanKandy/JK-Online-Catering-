2b9d0a83cfc42a8c4891b7709c9717329bf27781
JehanKandy <jehankandy@gmail.com>
2023-01-19 18:57:20 +0530

    Update reg.php

--- lib/views/reg.php
+++ lib/views/reg.php
@@ -2,6 +2,11 @@
 <?php include("../layouts/header.php"); ?>
 <?php include("../layouts/login_nav.php"); ?>
 
+<div class="reg-content">
+    <div class="container">
+        hi
+    </div>
+</div>
 
 <?php include("../layouts/page_footer.php"); ?>
 <script src="../../js/script.js"></script>
